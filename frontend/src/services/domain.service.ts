3ad374921b3c5f61714522755ef6cb2ac143c77f
https://github.com/divyayadav12 <divyayadav141203@gmail.com>
2026-06-20 09:59:51 +0530

    fix the error

--- frontend/src/services/domain.service.ts
+++ frontend/src/services/domain.service.ts
@@ -1,4 +1,4 @@
-const DOMAIN_API_BASE_URL = process.env.NEXT_PUBLIC_DOMAIN_API_BASE_URL ?? "http://127.0.0.1:8001";
+const DOMAIN_API_BASE_URL = process.env.NEXT_PUBLIC_DOMAIN_API_BASE_URL ?? "http://localhost:8001";
 
 import { api } from "./api";
 
